2d4be1a7a45f2ad2429f01474de6d44f80cc2972
skymaster11 <69782767+skymaster11@users.noreply.github.com>
2020-08-18 00:34:53 -0600

    Add files via upload

---
+++ includes/conexion.php
@@ -0,0 +1,6 @@
+<?php session_start();
+
+$conn = mysqli_connect("b0brcbvmvqcfvfxtxb5p-mysql.services.clever-cloud.com", "uf51tkrsr0kk4pf2", "yOq9ZcMtmPV8ZtbpO5e0");
+mysqli_select_db($conn, "b0brcbvmvqcfvfxtxb5p");
+
+?>
